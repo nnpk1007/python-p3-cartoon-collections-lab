f38a8a6dd06d54065817e6a11bcc0ef0dcba1407
Ngoc Nguyen <nnpk0410@gmail.com>
2023-07-07 14:08:20 -0500

    complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,29 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for dwarf in dwarves:
+        print(f"{dwarves.index(dwarf) + 1}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    calls = [f"{planeteer_call.capitalize()}!" for planeteer_call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+    return calls
 
-def find_the_cheese():
-    pass
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+a = summon_captain_planet(planeteer_calls)
+print(a)
+
+def long_planeteer_calls(planeteer_calls):
+    is_long = False
+
+    for planeteer_call in planeteer_calls:
+        if len(planeteer_call) >=5:
+            is_long = True
+            break
+    
+    return is_long
+
+# cheese_list = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(snacks):
+    for cheese in snacks:
+        if cheese in ("cheddar", "gouda", "camembert"):
+            return cheese
+    return None  
